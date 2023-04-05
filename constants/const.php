35bf5ee525e21c11822d3c3ef3f89c7a885b3cb2
Haseeb Akhtar <haseebhassy14@gmail.com>
2023-04-05 23:32:34 +0500

    updated gitignore

--- constants/const.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('base', 'http://localhost/hsbm-global/');
-
-define('version', "?v=" . (isset($_GET['rand']) ? rand() : '1'));
-define('img_version', "?v=" . (isset($_GET['rand']) ? rand() : '1'));
